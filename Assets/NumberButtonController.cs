1038fccffd45eddd368a71f2ff24c1ad80252d22
白鳥雅人 <whitesplash0703@icloud.com>
2019-08-22 22:19:31 +0900

    Lesson9課題
    遷移先の指定、ボタンの作成

---
+++ Assets/NumberButtonController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NumberButtonController : MonoBehaviour {
+    // ボタンのオブジェクトを宣言
+    public GameObject NumberButtonprefab;
+    // ボタンに設定する数値
+    public int Number;
+    // ボタンのサイズを設定
+    public int Width = 70;
+    public int Hight = 70;
+
+	// Use this for initialization
+	void Start () {
+        // ボタンのサイズを変更
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log(Number);
+            // クリックしたら消える
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/NumberButtonGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberButtonGenerator : MonoBehaviour {
+    // ボタンのオブジェクトを宣言
+    public GameObject NumberButtonPrefab;
+	// Use this for initialization
+	void Start () {
+        for (int i = 0; i < 10; i++)
+        {
+
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
